c2dbe288cedae0c64d6cc7f2ba7b473ce75cbd37
Mike <levinmk23@gmail.com>
2026-01-27 12:08:01 +0300

    Tests

--- src/main/java/hse/java/lectures/lecture1/Game.java
+++ src/main/java/hse/java/lectures/lecture1/Game.java
@@ -3,19 +3,26 @@ package hse.java.lectures.lecture1;
 import javax.swing.*;
 import java.awt.*;
 
-public class HelloWorld {
+public class Game {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("XO");
 
-        JFrame frame = new JFrame("Oh my God!!!");
         frame.setSize(300, 300);
+        frame.setLocation(200, 200);
+
+        JButton[][] buttons = new JButton[3][3];
 
         JPanel panel = new JPanel(new GridLayout(3,3));
         for (int i = 0; i < 9; i++) {
-            panel.add(new JButton());
+            JButton button = new JButton();
+            button.addActionListener(a -> {
+                button.setText("X");
+            });
+            panel.add(button);
         }
         frame.add(panel);
+        JDialog dialog;
         frame.setVisible(true);
-
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
--- src/test/java/hse/java/BaseTest.java
+++ src/test/java/hse/java/BaseTest.java
@@ -1,14 +1,11 @@
 package hse.java;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 
 public class BaseTest {
 
     @Test
     void test() {
-        int actually = 1;
-        Assertions.assertEquals(1, actually);
+        
     }
 }
