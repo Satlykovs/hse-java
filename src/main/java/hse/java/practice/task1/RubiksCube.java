b00d6532e2b98410865ce9cd437264699c24a38e
Satlykovs <satlykovs@gmail.com>
2026-02-06 14:29:15 +0300

    refactor: Hold my templates

--- src/main/java/hse/java/practice/task1/RubiksCube.java
+++ src/main/java/hse/java/practice/task1/RubiksCube.java
@@ -1,6 +1,7 @@
 package hse.java.practice.task1;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 
 /**
@@ -30,35 +31,64 @@ public class RubiksCube implements Cube
         }
     }
 
+    private void rotateSides(SideLine s0, SideLine s1, SideLine s2, SideLine s3)
+    {
+        CubeColor[] line0 = getLine(s0);
+        CubeColor[] line1 = getLine(s1);
+        CubeColor[] line2 = getLine(s2);
+        CubeColor[] line3 = getLine(s3);
+
+        setLine(s1, line0);
+        setLine(s2, line1);
+        setLine(s3, line2);
+        setLine(s0, line3);
+    }
+
+    private CubeColor[] getLine(SideLine s)
+    {
+        CubeColor[][] matrix = getMatrix(s.pos);
+        CubeColor[] res = new CubeColor[3];
+        for (int i = 0; i < 3; ++i)
+        {
+            res[i] = s.isRow ? matrix[s.idx][i] : matrix[i][s.idx];
+        }
+        if (s.rev)
+        {
+            CubeColor tmp = res[0];
+            res[0] = res[2];
+            res[2] = tmp;
+        }
+        return res;
+    }
+
+    private void setLine(SideLine s, CubeColor[] vals)
+    {
+        CubeColor[][] matrix = getMatrix(s.pos);
+        CubeColor[] data = s.rev ? new CubeColor[]{vals[2], vals[1], vals[0]} : vals;
+
+        for (int i = 0; i < 3; ++i)
+        {
+            if (s.isRow) matrix[s.idx][i] = data[i];
+            else matrix[i][s.idx] = data[i];
+        }
+    }
+
     @Override
     public void up(RotateDirection direction)
     {
         if (direction == RotateDirection.COUNTERCLOCKWISE)
         {
-            up(RotateDirection.CLOCKWISE);
-            up(RotateDirection.CLOCKWISE);
-            up(RotateDirection.CLOCKWISE);
+            rotate3(this::up);
             return;
         }
 
         rotateFaceMatrix(EdgePosition.UP);
-
-        CubeColor[][] front = getMatrix(EdgePosition.FRONT);
-        CubeColor[][] left = getMatrix(EdgePosition.LEFT);
-        CubeColor[][] back = getMatrix(EdgePosition.BACK);
-        CubeColor[][] right = getMatrix(EdgePosition.RIGHT);
-
-        CubeColor[] tmp = new CubeColor[3];
-
-        for (int i = 0; i < 3; ++i) tmp[i] = front[0][i];
-
-        for (int i = 0; i < 3; ++i) front[0][i] = right[0][i];
-
-        for (int i = 0; i < 3; ++i) right[0][i] = back[0][i];
-
-        for (int i = 0; i < 3; ++i) back[0][i] = left[0][i];
-
-        for (int i = 0; i < 3; ++i) left[0][i] = tmp[i];
+        rotateSides(
+                new SideLine(EdgePosition.FRONT, 0, true, false),
+                new SideLine(EdgePosition.LEFT, 0, true, false),
+                new SideLine(EdgePosition.BACK, 0, true, false),
+                new SideLine(EdgePosition.RIGHT, 0, true, false)
+        );
     }
 
     @Override
@@ -66,30 +96,18 @@ public class RubiksCube implements Cube
     {
         if (direction == RotateDirection.COUNTERCLOCKWISE)
         {
-            down(RotateDirection.CLOCKWISE);
-            down(RotateDirection.CLOCKWISE);
-            down(RotateDirection.CLOCKWISE);
+            rotate3(this::down);
             return;
         }
 
         rotateFaceMatrix(EdgePosition.DOWN);
 
-        CubeColor[][] front = getMatrix(EdgePosition.FRONT);
-        CubeColor[][] right = getMatrix(EdgePosition.RIGHT);
-        CubeColor[][] back = getMatrix(EdgePosition.BACK);
-        CubeColor[][] left = getMatrix(EdgePosition.LEFT);
-
-        CubeColor[] tmp = new CubeColor[3];
-
-        for (int i = 0; i < 3; ++i) tmp[i] = front[2][i];
-
-        for (int i = 0; i < 3; ++i) front[2][i] = left[2][i];
-
-        for (int i = 0; i < 3; ++i) left[2][i] = back[2][i];
-
-        for (int i = 0; i < 3; ++i) back[2][i] = right[2][i];
-
-        for (int i = 0; i < 3; ++i) right[2][i] = tmp[i];
+        rotateSides(
+                new SideLine(EdgePosition.FRONT, 2, true, false),
+                new SideLine(EdgePosition.RIGHT, 2, true, false),
+                new SideLine(EdgePosition.BACK, 2, true, false),
+                new SideLine(EdgePosition.LEFT, 2, true, false)
+        );
 
     }
 
@@ -98,92 +116,56 @@ public class RubiksCube implements Cube
     {
         if (direction == RotateDirection.COUNTERCLOCKWISE)
         {
-            left(RotateDirection.CLOCKWISE);
-            left(RotateDirection.CLOCKWISE);
-            left(RotateDirection.CLOCKWISE);
+            rotate3(this::left);
             return;
         }
 
         rotateFaceMatrix(EdgePosition.LEFT);
 
-        CubeColor[][] up = getMatrix(EdgePosition.UP);
-        CubeColor[][] front = getMatrix(EdgePosition.FRONT);
-        CubeColor[][] down = getMatrix(EdgePosition.DOWN);
-        CubeColor[][] back = getMatrix(EdgePosition.BACK);
-
-        CubeColor[] tmp = new CubeColor[3];
-
-        for (int i = 0; i < 3; ++i) tmp[i] = up[i][0];
-
-        for (int i = 0; i < 3; i++) up[i][0] = back[2 - i][2];
-
-        for (int i = 0; i < 3; i++) back[i][2] = down[2 - i][0];
-
-        for (int i = 0; i < 3; i++) down[i][0] = front[i][0];
-
-        for (int i = 0; i < 3; i++) front[i][0] = tmp[i];
+        rotateSides(
+                new SideLine(EdgePosition.UP, 0, false, false),
+                new SideLine(EdgePosition.FRONT, 0, false, false),
+                new SideLine(EdgePosition.DOWN, 0, false, false),
+                new SideLine(EdgePosition.BACK, 2, false, true)
+        );
 
     }
 
     @Override
-    public void right(RotateDirection direction) {
-        if (direction == RotateDirection.COUNTERCLOCKWISE) {
-            right(RotateDirection.CLOCKWISE);
-            right(RotateDirection.CLOCKWISE);
-            right(RotateDirection.CLOCKWISE);
+    public void right(RotateDirection direction)
+    {
+        if (direction == RotateDirection.COUNTERCLOCKWISE)
+        {
+            rotate3(this::right);
             return;
         }
 
         rotateFaceMatrix(EdgePosition.RIGHT);
 
-
-        CubeColor[][] up = getMatrix(EdgePosition.UP);
-        CubeColor[][] back = getMatrix(EdgePosition.BACK);
-        CubeColor[][] down = getMatrix(EdgePosition.DOWN);
-        CubeColor[][] front = getMatrix(EdgePosition.FRONT);
-
-        CubeColor[] temp = new CubeColor[3];
-
-        for (int i = 0; i < 3; i++) temp[i] = up[i][2];
-
-        for (int i = 0; i < 3; i++) up[i][2] = front[i][2];
-
-        for (int i = 0; i < 3; i++) front[i][2] = down[i][2];
-
-        for (int i = 0; i < 3; i++) down[i][2] = back[2 - i][0];
-
-        for (int i = 0; i < 3; i++) back[i][0] = temp[2 - i];
+        rotateSides(
+                new SideLine(EdgePosition.UP, 2, false, false),
+                new SideLine(EdgePosition.BACK, 0, false, true),
+                new SideLine(EdgePosition.DOWN, 2, false, false),
+                new SideLine(EdgePosition.FRONT, 2, false, false)
+        );
     }
 
     public void front(RotateDirection direction)
     {
         if (direction == RotateDirection.COUNTERCLOCKWISE)
         {
-            front(RotateDirection.CLOCKWISE);
-            front(RotateDirection.CLOCKWISE);
-            front(RotateDirection.CLOCKWISE);
+            rotate3(this::front);
             return;
         }
 
         rotateFaceMatrix(EdgePosition.FRONT);
 
-        CubeColor[][] up = getMatrix(EdgePosition.UP);
-        CubeColor[][] right = getMatrix(EdgePosition.RIGHT);
-        CubeColor[][] down = getMatrix(EdgePosition.DOWN);
-        CubeColor[][] left = getMatrix(EdgePosition.LEFT);
-
-
-        CubeColor[] tmp = new CubeColor[3];
-
-        for (int i = 0; i < 3; ++i) tmp[i] = up[2][i];
-
-        for (int i = 0; i < 3; ++i) up[2][i] = left[2 - i][2];
-
-        for (int i = 0; i < 3; ++i) left[i][2] = down[0][i];
-
-        for (int i = 0; i < 3; ++i) down[0][i] = right[2 - i][0];
-
-        for (int i = 0; i < 3; ++i) right[i][0] = tmp[i];
+        rotateSides(
+                new SideLine(EdgePosition.UP, 2, true, false),
+                new SideLine(EdgePosition.RIGHT, 0, false, false),
+                new SideLine(EdgePosition.DOWN, 0, true, true),
+                new SideLine(EdgePosition.LEFT, 2, false, true)
+        );
 
     }
 
@@ -192,30 +174,18 @@ public class RubiksCube implements Cube
     {
         if (direction == RotateDirection.COUNTERCLOCKWISE)
         {
-            back(RotateDirection.CLOCKWISE);
-            back(RotateDirection.CLOCKWISE);
-            back(RotateDirection.CLOCKWISE);
+            rotate3(this::back);
             return;
         }
 
         rotateFaceMatrix(EdgePosition.BACK);
 
-        CubeColor[][] up = getMatrix(EdgePosition.UP);
-        CubeColor[][] left = getMatrix(EdgePosition.LEFT);
-        CubeColor[][] down = getMatrix(EdgePosition.DOWN);
-        CubeColor[][] right = getMatrix(EdgePosition.RIGHT);
-
-        CubeColor[] tmp = new CubeColor[3];
-
-        for (int i = 0; i < 3; ++i) tmp[i] = up[0][i];
-
-        for (int i = 0; i < 3; ++i) up[0][i] = right[i][2];
-
-        for (int i = 0; i < 3; ++i) right[i][2] = down[2][2 - i];
-
-        for (int i = 0; i < 3; ++i) down[2][i] = left[i][0];
-
-        for (int i = 0; i < 3; ++i) left[i][0] = tmp[2 - i];
+        rotateSides(
+                new SideLine(EdgePosition.UP, 0, true, true),
+                new SideLine(EdgePosition.LEFT, 0, false, false),
+                new SideLine(EdgePosition.DOWN, 2, true, false),
+                new SideLine(EdgePosition.RIGHT, 2, false, true)
+        );
     }
 
     public Edge[] getEdges()
@@ -228,7 +198,6 @@ public class RubiksCube implements Cube
     {
         return Arrays.toString(edges);
     }
-
 
     private CubeColor[][] getMatrix(EdgePosition pos)
     {
@@ -243,7 +212,6 @@ public class RubiksCube implements Cube
         reverseRows(matrix);
 
     }
-
 
     private void transpose(CubeColor[][] matrix)
     {
@@ -267,4 +235,29 @@ public class RubiksCube implements Cube
             matrix[i][2] = tmp;
         }
     }
+
+    private void rotate3(Consumer<RotateDirection> f)
+    {
+        f.accept(RotateDirection.CLOCKWISE);
+        f.accept(RotateDirection.CLOCKWISE);
+        f.accept(RotateDirection.CLOCKWISE);
+    }
+
+    private static class SideLine
+    {
+        EdgePosition pos;
+        int idx;
+        boolean isRow;
+        boolean rev;
+
+        SideLine(EdgePosition p, int i, boolean row, boolean r)
+        {
+            pos = p;
+            idx = i;
+            isRow = row;
+            rev = r;
+        }
+    }
+
+
 }
